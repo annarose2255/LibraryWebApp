98c272c99617a31410c979db9fbc1646855d70fa
EmmaDuf <emmakdufresne@gmail.com>
2021-12-20 09:51:33 -0600

    Global Login Model

---
+++ WebApplicationExample/Models/RegisterModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LibraryWebApp.Models
+{
+    public class RegisterModel : BaseModel
+    {
+
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; }
+        public string PrimaryEmail { get; set; } = "";
+        public string PrimaryPhone { get; set; } = "";
+        public int RoleId { get; set; }
+        public int AddressID { get; set; }
+
+
+    }
+}
