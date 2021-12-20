1c6773a640de718e1d5fbbcc5137676f078011ce
EmmaDuf <emmakdufresne@gmail.com>
2021-12-20 07:39:20 -0600

    backend register functions

--- BusinessLayerExample/LoginBLL.cs
+++ BusinessLayerExample/LoginBLL.cs
@@ -52,4 +52,51 @@ namespace LibraryBusinessLogicLayer
 
         }
     }
+    public class RegisterBLL
+    {
+        public UserDTO Register(string username, string firstname, string lastname,string password, int roleid, int addressid, BusinessLogicPassThru businessLogicPassThru,string email = "",string phonenumber = "")
+        {
+            UserDTO _user;
+            // 1. get all the users for the database
+            List<UserDTO> _allUsers = businessLogicPassThru.GetUsersData();
+
+            // 2. filter thru and see if I have match for this username
+            UserDTO _match = (_allUsers.Where(u => u.Username == username).Count() == 1) ?
+                _allUsers.Where(u => u.Username == username).FirstOrDefault() : new UserDTO();
+
+
+            if (_match.Password != null)
+            {
+                // 3. if match,  hash and salt the password parm.
+                _match.ErrorMessage = "Username already exists.";
+                return _match;
+            }
+            else
+            {
+                //create new user from HTTPGet 
+                _user = new UserDTO
+                {
+                    //TODO: will need to change how roleid and addressid are passed through, currently no RoleDTO
+                    RoleId = roleid,
+                    AddressID = addressid,
+                    FirstName = firstname,
+                    LastName = lastname,
+                    PrimaryEmail = email,
+                    PrimaryPhone = phonenumber,
+                    Username = username,
+                    Password = password,
+                    Salt = "",//use password to get salt 
+                    Comment = "",
+                    DateModified = DateTime.Now,
+                    ModifiedByUserID = roleid
+
+                };
+                //assign userid after creating user in Db
+                int userid = businessLogicPassThru.CreateUser(_user);
+                _user.UserId = userid;
+                return _user;
+            }
+            //return _newuser;
+        }
+    }
 }
