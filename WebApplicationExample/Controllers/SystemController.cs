02738d96dc0ac030c15a3235490ffaef779c8d15
Giancarlo Rhodes <giancarlo.rhodes@onshoreoutsourcing.com>
2021-12-21 15:40:18 -0600

    working on editor DataTables

--- WebApplicationExample/Controllers/SystemController.cs
+++ WebApplicationExample/Controllers/SystemController.cs
@@ -4,17 +4,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataTables;
+using LibraryWebApp.Models;
+using LibraryBusinessLogicLayer;
 
 namespace WebApplicationExample.Controllers
 {
+   
+
     public class SystemController : Controller
     {
-        // GET: System
+        [HttpGet]
         public ActionResult Dashboard()
         {
 
-            UserDTO _profile  = (UserDTO)System.Web.HttpContext.Current.Session["Profile"];
+            // TODO: remove after testing 
+            UserDTO _profile = new UserDTO
+            {
+                UserId = 1,
+                Username = "grhodes29",
+                FirstName = "Giancarlo",
+                LastName = "Rhodes",
+                RoleId = 1,
+                RoleName = "Administrator"
+            };
+            // UserDTO _profile
+            System.Web.HttpContext.Current.Session["Profile"] = _profile;
             return View();
         }
+
+        //[AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]
+        //public ActionResult Table()
+        //{
+        //    // connection string coming out of the web.config
+        //    BusinessLogicPassThru businessLogicPassThru = 
+        //        new BusinessLogicPassThru(System.Configuration.ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString);
+        //    string _dbConnection = System.Configuration.ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+        //    string _dbType = "Sqlserver";
+
+        //    //var settings = Properties.Settings.Default;
+        //    var formData = HttpContext.Request.Form;
+
+        //    using (var db = new Database(_dbType, _dbConnection))
+        //    {
+        //        var response = new Editor(db, "staff")
+        //            .Model<LoginModel>()
+        //            .Field(new Field("start_date")
+        //                .Validator(Validation.DateFormat(
+        //                    Format.DATE_ISO_8601,
+        //                    new ValidationOpts { Message = "Please enter a date in the format yyyy-mm-dd" }
+        //                ))
+        //                .GetFormatter(Format.DateSqlToFormat(Format.DATE_ISO_8601))
+        //                .SetFormatter(Format.DateFormatToSql(Format.DATE_ISO_8601))
+        //            )
+        //            .Process(formData)
+        //            .Data();
+
+
+        //        // TODO: add to db here
+
+        //        return Json(response, JsonRequestBehavior.AllowGet);
+        //    }
+        //}
     }
+
 }
