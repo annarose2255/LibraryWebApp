412c14150b77482ffff833170699ade630f88fcf
EmmaDuf <emmakdufresne@gmail.com>
2021-12-21 11:30:38 -0600

    Register phone and email

--- WebApplicationExample/Utility/Mapper.cs
+++ WebApplicationExample/Utility/Mapper.cs
@@ -15,14 +15,14 @@ namespace WebApplicationExample.Utility
 
         public static UserDTO GlobalLoginModelToUserDTO(GlobalLoginModel inModel)
         {
-            UserDTO _userDTO = new UserDTO 
-            { 
+            UserDTO _userDTO = new UserDTO
+            {
                 Username = inModel.RegisterModel.Username,
                 FirstName = inModel.RegisterModel.FirstName,
                 LastName = inModel.RegisterModel.LastName,
                 Password = inModel.RegisterModel.Password,
-                PrimaryEmail = inModel.RegisterModel.PrimaryEmail,
-                PrimaryPhone = inModel.RegisterModel.PrimaryPhone,
+                PrimaryEmail = inModel.RegisterModel.PrimaryEmail is null ? "" : inModel.RegisterModel.PrimaryEmail,
+                PrimaryPhone = inModel.RegisterModel.PrimaryPhone is null ? "": inModel.RegisterModel.PrimaryPhone,
                 RoleId = (int)RoleType.Member, // this is the default
                 RoleName = RoleType.Member.ToString()
             };
