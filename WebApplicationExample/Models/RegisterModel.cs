2f5931883183ca063a35199e02f09e7dc9f85f1e
annarose2255 <daytonkaroll@outlook.com>
2021-12-29 14:40:35 -0500

    did login and register password verifcation

--- WebApplicationExample/Models/RegisterModel.cs
+++ WebApplicationExample/Models/RegisterModel.cs
@@ -14,6 +14,12 @@ namespace LibraryWebApp.Models
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[#@$%!?^&*\-+]).*$", ErrorMessage = "Invalid Password. Passwords must contain: " +
+            "<br> &ensp; \u2022 One or more captial letters " +
+            "<br> &ensp; \u2022 One or more lower case letters" +
+            "<br> &ensp; \u2022 One or more numbers (0-9)" +
+            "<br> &ensp; \u2022 One or more special characters (#, ?, !, @, $, %, ^, &, *, -, +)")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Password must be at least 8 and no more than 20 characters long.")]
         public string Password { get; set; }
 
 
